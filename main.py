436e6bb2531b1299f31b3f6ecbb0e3698c671c2b
Juan David Castillo Caraballo <juan.castillo.c@uniautonoma.edu.co>
2025-05-05 12:41:23 -0500

    Se incluyo la función de las preguntas en la opcion de nivel seleccionada

--- main.py
+++ main.py
@@ -35,45 +35,51 @@ def main():
 
     while True:
         mostrar_menu()
-        opcion=int(input("Seleccione la opción deseada [1-5]"))
-
+        try:
+                opcion=int(input("Seleccione la opción deseada [1-5]"))
+        except ValueError:
+              print("Por favor, ingrese un numero del 1 al 5")
+              continue
+                        
+                        
         if opcion==1:
-                print(f"Inicializando quiz para {nombre_Usuario}")
-                print("Bienvenido al curso express de aprendizaje python")
-                print("Selecciona el nivel que deseas aprender")
-                print("1. Nivel Básico de pyhton")
-                print("2. Nivel medio de pyhton")
-                print("3. Nivel experto de pyhton")
-                opcion=int(input("Selecciona una opcion"))
+                                print(f"Inicializando quiz para {nombre_Usuario}")
+                                print("Bienvenido al curso express de aprendizaje python")
+                                print("Selecciona el nivel que deseas aprender")
+                                print("1. Nivel Básico de pyhton")
+                                print("2. Nivel medio de pyhton")
+                                print("3. Nivel experto de pyhton")
+                                nivel=int(input("Selecciona un nivel"))
 
-                if opcion==1:
-                       print("Entraste al cuestionario basico de python ¡Preparate!")
-                       print("A continuación te realizaremos una serie de preguntas las cuales determinaran si tu nivel de python es el que indicaste " \
-                       "o si perteneces a un nivel mas avanzado, dichas preguntas te daran un puntaje y poco a poco iran aumentando su dificultad.")
-               
-               
-               
-                elif opcion==2:
-                       print("Entraste al cuestionario medio de python ¡Preparate!")
-                elif opcion==3:
-                       print("Entraste al cuestionario experto de python ¡Preparate!")
-                #Inicializar el Quiz
-                #funciones para gestionar las preguntas
+                                if nivel==1:
+                                        print("Entraste al cuestionario basico de python ¡Preparate!")
+                                        print("A continuación te realizaremos una serie de preguntas las cuales determinaran si tu nivel de python es el que indicaste " \
+                                        "o si perteneces a un nivel mas avanzado, dichas preguntas te daran un puntaje y poco a poco iran aumentando su dificultad.")
+                                                
+                                        ejecutar_quiz_basico()
+                        
+                        
         elif opcion==2:
-                print(f"Mostrando estadisticas de jugador {nombre_Usuario}")
-                #Mostrar estadisticas en pantalla
-                #
+                                print("Entraste al cuestionario medio de python ¡Preparate!")
         elif opcion==3:
-                print(f"Mostrando estadisticas del jugador {nombre_Usuario}")
-                #Mostrar historial del jugador
+                                print("Entraste al cuestionario experto de python ¡Preparate!")
+                                #Inicializar el Quiz
+                                #funciones para gestionar las preguntas
+        elif opcion==2:
+                                print(f"Mostrando estadisticas de jugador {nombre_Usuario}")
+                                #Mostrar estadisticas en pantalla
+                                #
+        elif opcion==3:
+                                print(f"Mostrando estadisticas del jugador {nombre_Usuario}")
+                                #Mostrar historial del jugador
         elif opcion==4:
-                print(f"Cambiando nombre del usuario a {nombre_Usuario}")
-                #Cambiar el nombre de usuario 
+                                print(f"Cambiando nombre del usuario a {nombre_Usuario}")
+                                #Cambiar el nombre de usuario 
         elif opcion==5:
-                print("Saliendo del programa, Adiós")
-                break
+                                print("Saliendo del programa, Adiós")
+                                break
         else:
-                print("Opción no válida")
+                                print("Opción no válida")
 
 main()
             
